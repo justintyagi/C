8de173b0fe523b06c3eae6e3d301c946f8692dec
justintyagi <justintyagi292@gmail.com>
2025-06-25 11:15:38 +0530

    initial commit

--- function.c
+++ function.c
@@ -1,20 +1,31 @@
+// # include<stdio.h>
+// int sum(int a,int b){
+//     n=2;
+//     return
+// }
+
+// int main(){
+//     int a,b;
+
+//     printf("enter the first number ");
+//     scanf("%d",&a);
+//     printf("enter the second number b");
+//     scanf("%d",&b);
+//    int s= sum(a,b);
+//    printf("sum is %d",s);
+//     return 0;
+// }
+// int sum(int a,int b){
+//     return a+b;
+// }
+
 # include<stdio.h>
-int sum(int a,int b){
-    n=2;
-    return
+written(int a){
+    int a=5;
+    return a;
+
 }
-
-int main(){
-    int a,b;
-
-    printf("enter the first number ");
-    scanf("%d",&a);
-    printf("enter the second number b");
-    scanf("%d",&b);
-   int s= sum(a,b);
-   printf("sum is %d",s);
+int main()[
+    written(a)
     return 0;
-}
-int sum(int a,int b){
-    return a+b;
-}
+]
--- recursion.c
+++ recursion.c
@@ -12,36 +12,20 @@ int main(void){
     printf("sum is : %d",sum(5));
     return 0;
 }*/
-// # include<stdio.h>
-// int fact(int n){
-//     if(n==1){
-//         return 1;
-//     }
-//     int factNm1 = fact(n-1);
-//     int factN = factNm1*n;
-//   return factN;
-// }
-// int main(){
-//     printf("factorial is : %d",fact(5));
-//     return 0;
-// }
-
-
-
-// int n, first = 0, second = 1, next;
-
-//     printf("Enter the number of terms: ");
-//     scanf("%d", &n);
-
-//     printf("Fibonacci Series: ");
-
-//     for (int i = 0; i < n; i++) {
-//         printf("%d ", first);
-//        
-
 # include<stdio.h>
-int fabonicseries (int n, int first,int second,int next){
-          next = first + second;
-          first = second;
-          second = next;
-} 
+int fact(int n){
+    if(n==1){
+        return 1;
+    }
+    int factNm1 = fact(n-1);
+    int factN = factNm1*n;
+  return factN;
+}
+int main(){
+    printf("factorial is : %d",fact(5));
+    return 0;
+}
+
+
+
+
